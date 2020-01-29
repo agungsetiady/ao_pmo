e81033cbde264da52ea28b7699ab003e04ba043f
Agung Setiady <setiadyagung@gmail.com>
2020-01-30 02:27:18 +0700

    Minor Update

--- resources/views/layouts/partials/lang-switcher.blade.php
+++ resources/views/layouts/partials/lang-switcher.blade.php
@@ -1,7 +1,7 @@
 <div class="text-center" style="border-bottom: 1px solid #e7e7e7; padding-bottom: 10px">
     {{ trans('lang.lang') }} :
 
-    @foreach (['en', 'id', 'de'] as $langKey)
+    @foreach (['en', 'id'] as $langKey)
         {!! FormField::formButton(
             [
                 'route'  => 'users.profile.switch-lang',
